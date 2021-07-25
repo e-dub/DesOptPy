52ae788413e4602a4be031b9ef7b2f269aba82aa
wehrle <Erich.Wehrle@unibz.it>
2021-07-25 16:23:11 +0200

    readme figs

--- DesOptPy/DesOptPy.py
+++ DesOptPy/DesOptPy.py
@@ -83,41 +83,8 @@ def OptimizationProblem(Model):
             cpu = None
 
         #class KaruschKuhnTucker:
-
-        # THIs is ugly, cannot I not add plotting.Plotconvergence to this object and have only once here?
-        def plotConvergence(self, show=True, savePDF=False,savePNG=False,
-                            saveSVG=False, saveTikZ=False):
-             plotting.plotConvergence(self,  show,  savePDF, savePNG, saveSVG,
-                                      saveTikZ)
-
-        def checkKKT(self):
-            from numpy.linalg import norm, lstsq, pinv
-            self.kkteps = 1e-3
-            iActive = list(np.array(self.gOpt) > -self.kkteps).index(True)
-            if len(iActive) == 1:
-               lam= np.divide(np.array(self.fNablaIt[-1]),
-                              np.array(self.gNablaIt[-1]).reshape(5,2)[iActive,:])
-               Lambda = np.average(lam)
-            self.PrimalFeas = (max(self.gOpt) < self.kkteps)
-            self.ComplSlack = (max(self.gOpt@self.Lambda) < self.kkteps)
-            self.DualFeas = (min(self.Lambda) > -self.kkteps)
-            self.kktOpt = bool(self.PrimalFeas*self.DualFeas*self.ComplSlack)
-            self.Opt1Order = np.linalg.norm(self.OptResidual)
-            self.kktMax = max(abs(self.OptResidual))
-            if np.size(lambda_c) > 0:
-                print("Lagrangian multipliers = " +
-                str(lambda_c.reshape(np.size(lambda_c,))))
-                print("Type of active constraints = " + str(gAllActiveType))
-                print("Shadow prices = " + str(SPg))
-            if kktOpt:
-                print("Karush-Kuhn-Tucker optimality criteria fulfilled")
-            elif kktOpt==0:
-                print("Karush-Kuhn-Tucker optimality criteria NOT fulfilled")
-            if Opt1Order:
-                print("First-order residual of Lagrangian function = " + str(Opt1Order))
-
-        def calcPosprocessing(self):
-            self.ShadowPrices = []
+        from DesOptPy.plotting import plotConvergence
+        from DesOptPy.PostprocessingNumerical import checkKKT, calcShadowPrices
 
         def readHistory(self):
             # make function of new file
@@ -622,15 +589,10 @@ def OptimizationProblem(Model):
                 from DesOptPy.interfaces import NlOpt
                 NlOpt.OptNlOpt(self, x0, xL, xU, SysEq)
 
-
-
             def optimizeMutiobjective(self):
-
-
-
-
-#----------------------------------------------
                 pass
+
+
 # Optimization over...now postprocessing and such
             self.t1 = datetime.datetime.now()
             self.tOpt = (self.t1-self.t0)
@@ -642,4 +604,5 @@ def OptimizationProblem(Model):
                 os.chdir(self.MainDir)
                 if self.RemoveRunFolder:
                     shutil.rmtree(self.RunDir)
+
     return Opt()
