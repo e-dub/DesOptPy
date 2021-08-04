9bdf0ecac84888321bf6991236c020efa65af66f
wehrle <Erich.Wehrle@unibz.it>
2021-08-04 17:42:04 +0200

    a bit monitor, readme

--- DesOptPy/Monitor.py
+++ DesOptPy/Monitor.py
@@ -17,23 +17,35 @@ https://github.com/holoviz/panel/issues/1496
 
 # ds = ColumnDataSource({"x": [0], "fAll": [0], "gMax": [0]})
 
-showPlot = False
+showPlot = True
 savePlot = False
 Name = os.getcwd() + os.sep + os.getcwd().split(os.sep)[-1][6:]
-colors10 = [
-    "#1f77b4",
-    "#ff7f0e",
-    "#2ca02c",
-    "#d62728",
-    "#9467bd",
-    "#8c564b",
-    "#e377c2",
-    "#7f7f7f",
-    "#bcbd22",
-    "#17becf",
-]
+
 font = "palantino"
 fontSize = "12pt"
+
+def colorPalette(nColors):
+    # Palettes: https://docs.bokeh.org/en/latest/docs/reference/palettes.html
+    if nx < 11:
+        colorsx = palettes.d3["Category10"][10]
+    elif nx < 21:
+        colorsx = palettes.d3["Category20"][20][::2] + palettes.d3["Category20"][20][1::2]
+    else:
+        colorsx = (
+            palettes.d3["Category20"][20][::2]
+            + palettes.d3["Category20"][20][1::2]
+            + palettes.d3["Category20c"][20][::4]
+            + palettes.d3["Category20b"][20][::4]
+            + palettes.d3["Category20c"][20][1::4]
+            + palettes.d3["Category20b"][20][1::4]
+            + palettes.d3["Category20c"][20][2::4]
+            + palettes.d3["Category20b"][20][2::4]
+            + palettes.d3["Category20c"][20][3::4]
+            + palettes.d3["Category20b"][20][3::4]
+        )
+    if nx > 60:
+        colorsx *= nx // 60 + 1
+    return(colorsx[:nColors])
 
 
 def readHistory():
@@ -47,7 +59,7 @@ def readHistory():
     return (xAll, fAll, gAll, gMax)
 
 
-def plotStyle(p, axes):
+def plotStyle(p, axes, colors):
     # colors
     p.xgrid.visible = False
     p.ygrid.visible = False
@@ -85,14 +97,14 @@ def plotStyle(p, axes):
     p.yaxis.major_label_text_font_style = "normal"
     p.yaxis.major_label_text_font_size = fontSize
     if axes == 2:
-        p.yaxis[0].axis_label_text_color = colors10[0]
-        p.yaxis[1].axis_label_text_color = colors10[3]
-        p.yaxis[0].major_label_text_color = colors10[0]
-        p.yaxis[1].major_label_text_color = colors10[3]
-        p.yaxis[0].major_tick_line_color = colors10[0]
-        p.yaxis[1].major_tick_line_color = colors10[3]
-        p.yaxis[0].axis_line_color = colors10[0]
-        p.yaxis[1].axis_line_color = colors10[3]
+        p.yaxis[0].axis_label_text_color = colors[0]
+        p.yaxis[1].axis_label_text_color = colors[3]
+        p.yaxis[0].major_label_text_color = colors[0]
+        p.yaxis[1].major_label_text_color = colors[3]
+        p.yaxis[0].major_tick_line_color = colors[0]
+        p.yaxis[1].major_tick_line_color = colors[3]
+        p.yaxis[0].axis_line_color = colors[0]
+        p.yaxis[1].axis_line_color = colors[3]
     p.yaxis.major_tick_in = 5
     p.yaxis.major_tick_out = 0
     p.yaxis.minor_tick_in = 0  # 2
@@ -126,10 +138,18 @@ def update(event=None):
         ds_x[i].stream(
             {
                 "x": [it[-1]],
-                # "xAll": np.array(xAll).reshape(nx, len(it)).tolist(),
+                # "xAll": np.array(xAlcolorPalette(4)l).reshape(nx, len(it)).tolist(),
                 "xAll": [xAll[-1][i]],
             }
         )
+
+    for i in range(ng):
+        ds_g[i].stream(
+        {
+            "x": [it[-1]],
+            "gAll": [gAll[-1][i]],
+        }
+    )
 
 
 # initial
@@ -172,27 +192,28 @@ p_fg.y_range = Range1d(
 # p_fg.y_range.range_padding = fDelta[0]*0.01
 # p_fg.y_range.range_padding = fDelta[0]*0.01
 
+colors_fg = colorPalette(10)
 # plot style
-p_fg = plotStyle(p_fg, 2)
+p_fg = plotStyle(p_fg, 2, colors_fg)
 p_fg.yaxis[0].axis_label = "objective value"
 p_fg.yaxis[1].axis_label = "maximum constraint value"
 
 
 # plot
-p_fg.line(x="x", y="fAll", line_color=colors10[0], source=ds_fg)
+p_fg.line(x="x", y="fAll", line_color=colors_fg[0], source=ds_fg)
 p_fg.circle(
     x="x", y="fAll", fill_color="white", line_color="white", source=ds_fg, size=10
 )
 p_fg.circle(
     x="x",
     y="fAll",
-    fill_color=colors10[0],
-    line_color=colors10[0],
+    fill_color=colors_fg[0],
+    line_color=colors_fg[0],
     source=ds_fg,
     size=5,
 )
 p_fg.line(
-    x="x", y="gMax", line_color=colors10[3], y_range_name="constraint", source=ds_fg
+    x="x", y="gMax", line_color=colors_fg[3], y_range_name="constraint", source=ds_fg
 )
 p_fg.circle(
     x="x",
@@ -206,8 +227,8 @@ p_fg.circle(
 p_fg.circle(
     x="x",
     y="gMax",
-    fill_color=colors10[3],
-    line_color=colors10[3],
+    fill_color=colors_fg[3],
+    line_color=colors_fg[3],
     y_range_name="constraint",
     source=ds_fg,
 )
@@ -219,33 +240,15 @@ https://medium.com/@andrewm4894/bokeh-battles-part-1-multi-line-plots-311109992f
 https://towardsdatascience.com/draw-beautiful-and-interactive-line-charts-using-bokeh-in-python-9f3e11e0a16e
 """
 
-# Palettes: https://docs.bokeh.org/en/latest/docs/reference/palettes.html
-if nx > 11:
-    colorsx = palettes.d3["Category10"][10]
-elif nx > 21:
-    colorsx = palettes.d3["Category20"][20][::2] + palettes.d3["Category20"][20][1::2]
-else:
-    colorsx = (
-        palettes.d3["Category20c"][20][::4]
-        + palettes.d3["Category20b"][20][::4]
-        + palettes.d3["Category20c"][20][1::4]
-        + palettes.d3["Category20b"][20][1::4]
-        + palettes.d3["Category20c"][20][2::4]
-        + palettes.d3["Category20b"][20][2::4]
-        + palettes.d3["Category20c"][20][3::4]
-        + palettes.d3["Category20b"][20][3::4]
-    )
-if nx > 40:
-    colorsx *= nx // 40 + 1
-colorsx = colorsx[:nx]
+
 
 # initialize design variable convergence
 p_x = figure(sizing_mode="stretch_both", toolbar_location="above", toolbar_sticky=False)
 p_x.xaxis.axis_label = "evaluation"
 p_x.yaxis.axis_label = "normalized design variable value"
-
+colorsx = colorPalette(nx)
 # plot style
-p_x = plotStyle(p_x, 1)
+p_x = plotStyle(p_x, 1, colorsx)
 
 # plot
 # p_x.multi_line(
@@ -296,6 +299,67 @@ p_x.legend.border_line_alpha = 0.0
 p_x.legend.label_text_font = font
 p_x.legend.label_text_font_size = "12pt"
 
+
+# initialize design variable convergence
+ng = len(gAll[0])
+p_g = figure(sizing_mode="stretch_both", toolbar_location="above", toolbar_sticky=False)
+p_g.xaxis.axis_label = "evaluation"
+p_g.yaxis.axis_label = "constraint value"
+colors_g = colorPalette(ng)
+# plot style
+p_g = plotStyle(p_g, 1, colors_g)
+
+# plot
+# p_x.multi_line(
+#     xs="x",
+#     ys="xAll",
+#     source=ds_x,
+#     color=["red", "blue"],
+# )
+ds_g = [[]] * ng
+for i in range(ng):
+    ds_g[i] = ColumnDataSource(
+        {
+            "x": it.tolist(),
+            # "xAll": np.array(xAll).reshape(nx, len(it)).tolist(),
+            "gAll": np.array(gAll)[:, i].tolist(),
+        }
+    )
+
+    p_g.line(
+        x="x",
+        y="gAll",
+        source=ds_g[i],
+        color=colors_g[i],
+        legend_label="constraint " + str(i + 1),
+    )  # legend_label=r"$\\hat{x}_"+str(i+1)+"$")
+    p_g.circle(
+        x="x",
+        y="gAll",
+        source=ds_g[i],
+        legend_label="constraint " + str(i + 1),
+        fill_color="white",
+        line_color="white",
+        size=10,
+    )
+    p_g.circle(
+        x="x",
+        y="gAll",
+        legend_label="constraint " + str(i + 1),
+        source=ds_g[i],
+        color=colors_g[i],
+        size=5,
+    )
+
+p_g.add_layout(p_g.legend[0], "right")
+p_g.legend.click_policy = "hide"
+p_g.legend.background_fill_alpha = 0.0
+p_g.legend.border_line_alpha = 0.0
+p_g.legend.label_text_font = font
+p_g.legend.label_text_font_size = "12pt"
+
+
+
 # layout
 """
 https://docs.bokeh.org/en/latest/docs/user_guide/layout.html
@@ -304,6 +368,7 @@ plotLayout = layout(
     [
         [p_fg],
         [p_x],
+        [p_g],
     ],
     sizing_mode="stretch_both",
 )
@@ -311,7 +376,7 @@ plotLayout = layout(
 pane = pn.panel(plotLayout).servable()
 pn.state.add_periodic_callback(update, 100)
 
-if savePlot:
+if showPlot:
     show(plotLayout)
 
 if savePlot:
